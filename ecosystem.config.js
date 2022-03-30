6a28c8bcc7a4eb4a0578792d032fb5ac1b475eb9
Sungsu Park <sungsupark91@gmail.com>
2022-03-30 18:20:26 +0900

    로그 위치 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,22 @@ module.exports = {
             name: "수빈",
             script: "src/index.js",
             args: "soobin",
+            error_file: "log/soobin_err.log",
+            out_file: "log/soobin_out.log",
         },
         {
             name: "스냅",
             script: "src/index.js",
             args: "snap",
+            error_file: "log/snap_err.log",
+            out_file: "log/snap_out.log",
         },
         {
             name: "Raw",
             script: "src/index.js",
             args: "raw",
+            error_file: "log/raw_err.log",
+            out_file: "log/raw_out.log",
         },
     ],
 };
